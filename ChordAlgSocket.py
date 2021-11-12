c6091d69b8b1d8531010b85dd184beef5e7510ad
nitin-hugar <nhugar3@gatech.edu>
2021-11-12 14:10:37 -0500

    add chordAlgSocket and ChordAlgTrigger

---
+++ ChordAlgSocket.py
@@ -0,0 +1,131 @@
+# Author: Hardik Goel
+# Date Modified: 10/20/2021
+# Project: Generative Gestures
+
+
+from __future__ import print_function
+
+import logging
+import sys
+import time
+import socket
+from queue import Queue
+
+from rtmidi.midiconstants import NOTE_ON
+from rtmidi.midiutil import open_midiinput
+
+HOST = '0.0.0.0'  # The server's hostname or IP address
+PORT = 58299       # The port used by the server
+
+log = logging.getLogger('midiin_poll')
+logging.basicConfig(level=logging.DEBUG)
+
+class MyMidiHandler():
+
+    def __init__(self):
+        #################################### Setup MIDI ####################################
+        # Prompts user for MIDI input port, unless a valid port number or name
+        # is given as the first argument on the command line.
+        # API backend defaults to ALSA on Linux.
+
+        self.port = sys.argv[1] if len(sys.argv) > 1 else None
+        try:
+            self.midiin, self.port_name = open_midiinput(self.port)
+
+        except (EOFError, KeyboardInterrupt):
+            sys.exit()
+
+        self.NOTES = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
+        self.OCTAVES = list(range(11))
+        self.NOTES_IN_OCTAVE = len(self.NOTES)
+
+    def number_to_note(self, number: int) -> tuple:
+        octave = number // self.NOTES_IN_OCTAVE
+        assert octave in self.OCTAVES
+        assert 0 <= number <= 127
+        note = self.NOTES[number % self.NOTES_IN_OCTAVE]
+        return note, octave
+
+    def note_to_number(self, note: str, octave: int) -> int:
+        assert note in self.NOTES
+        assert octave in self.OCTAVES
+        note = self.NOTES.index(note)
+        note += (self.NOTES_IN_OCTAVE * octave)
+        assert 0 <= note <= 127
+        return note
+
+    def determineTriads(self, q):
+        pitch1 = q.get()
+        pitch2 = q.get()
+        pitch3 = q.get()
+
+        # sort pitches into a ascending order
+        pitches = [pitch1, pitch2, pitch3]
+        pitches_ascending = sorted(pitches)
+        print(pitches_ascending)
+        # find differences between pitches, normalizing across octaves
+        interval1 = ((pitches_ascending[1] - pitches_ascending[0]) % 12)
+        interval2 = ((pitches_ascending[2] - pitches_ascending[1]) % 12)
+
+        # check for major triad
+        if (interval1 == 4 and interval2 == 3) or (interval1 == 3 and interval2 == 5) or (
+                interval1 == 5 and interval2 == 4):
+            print("major")
+            return "major"
+        # check for minor triad
+        elif (interval1 == 3 and interval2 == 4) or (interval1 == 4 and interval2 == 5) or (
+                interval1 == 5 and interval2 == 3):
+            print("minor")
+            return "minor"
+        # check for augmented triad
+        elif (interval1 == 4 and interval2 == 4) or (interval1 == 4 and interval2 == 4) or (
+                interval1 == 4 and interval2 == 4):
+            print("augmented")
+            return "augmented"
+        # check for diminished triad
+        elif (interval1 == 3 and interval2 == 3) or (interval1 == 3 and interval2 == 6) or (
+                interval1 == 6 and interval2 == 3):
+            print("diminished")
+            return "diminished"
+
+        else:
+            return "none"
+
+    def on_midi_command(self):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(None)
+        s.connect((HOST, PORT))
+        s.settimeout(None)
+
+        q = Queue()
+
+        try:
+            while True:
+                event = self.midiin.get_message()
+                if event:
+                    message, deltatime = event
+                    if message[0] & 0xF0 == NOTE_ON:
+                        status, note, velocity = message
+                        # note = self.number_to_note(note)
+                        # print(note[0])
+                        if deltatime <= 0.5 and q.qsize() > 2:
+                            chord = self.determineTriads(q)
+                            q = Queue()
+                            s.sendall(chord.encode('UTF-8'))
+                        q.put(note)
+
+        except KeyboardInterrupt:
+            s.close()
+            print('Exiting')
+
+        finally:
+            s.close()
+            print("Exit.")
+            self.midiin.close_port()
+            del self.midiin
+
+if __name__ == "__main__":
+    midi = MyMidiHandler()
+    midi.on_midi_command()
+
+
---
+++ ChordAlgTrigger.py
@@ -0,0 +1,155 @@
+# Author: Hardik Goel
+# Date Modified: 10/20/2021
+# Project: Generative Gestures
+
+
+import os
+import sys
+import time
+import csv
+import pdb
+from queue import Queue
+from threading import Thread
+import time
+import socket
+
+SOCKET_HOST = '0.0.0.0'  # Standard loopback interface address (localhost)
+SOCKET_PORT = 58299        # Port to listen on (non-privileged ports are > 1023)
+
+def cvt(path):
+    return_vector = []
+    for i in range(7 * len(arms)):
+        return_vector.append(float(path[i]))
+    return return_vector
+
+
+def playDance(step):
+    length = len(step)
+    # for a in range (3):
+    #     print(2-a)
+    #     time.sleep(1)
+    for i in range(length):
+        start_time = time.time()
+        # board.digital[4].mode = pyfirmata.INPUT
+        # board.digital[2].mode = pyfirmata.INPUT
+        # red = board.digital[4].read()
+
+        j_angles = (step[i])
+        # b=6
+        # arms[b].set_servo_angle_j(angles=j_angles[(b * 7):((b + 1) * 7)], is_radian=False)
+
+        for b in range(totalArms):
+            arms[b].set_servo_angle_j(angles=j_angles[(b * 7):((b + 1) * 7)], is_radian=False)
+        tts = time.time() - start_time
+        sleep = 0.006 - tts
+
+        if tts > 0.006:
+            sleep = 0
+
+        print(tts)
+        time.sleep(sleep)
+
+
+def readFile(csvFile):
+    flower = []
+    with open(csvFile, newline='') as csvfile:
+        paths_reader_j = csv.reader(csvfile, delimiter=',', quotechar='|')
+        for path in paths_reader_j:
+            flower.append(cvt(path))
+    return flower
+
+
+def setup():
+    for a in arms:
+        a.set_simulation_robot(on_off=False)
+        # a.motion_enable(enable=True)
+        a.clean_warn()
+        a.clean_error()
+        a.set_mode(0)
+        a.set_state(0)
+        a.set_servo_angle(angle=[0.0, 0.0, 0.0, 1.57, 0.0, 0, 0.0], wait=False, speed=0.4, acceleration=0.25,
+                          is_radian=True)
+
+
+if __name__ == "__main__":
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((SOCKET_HOST, SOCKET_PORT))
+    s.listen()
+    s.settimeout(None)
+    conn, addr = s.accept()
+    s.settimeout(None)
+
+    ROBOT = "xArms"
+    PORT = 5004
+
+    sys.path.append(os.path.join(os.path.dirname(__file__), '../../..'))
+    from xarm.wrapper import XArmAPI
+
+    arm1 = XArmAPI('192.168.1.236')
+    arm2 = XArmAPI('192.168.1.203')
+    arm3 = XArmAPI('192.168.1.215')
+    arm4 = XArmAPI('192.168.1.244')
+    arm5 = XArmAPI('192.168.1.208')
+    arm6 = XArmAPI('192.168.1.242')
+    arm7 = XArmAPI('192.168.1.204')
+    arm8 = XArmAPI('192.168.1.226')
+    arm9 = XArmAPI('192.168.1.234')
+    arm10 = XArmAPI('192.168.1.237')
+
+    arms = [arm1, arm2, arm3, arm4, arm5, arm6, arm7, arm8, arm9, arm10]
+    totalArms = len(arms)
+
+    # Using relative path to retrieve dances
+    directory = os.path.join(os.path.dirname(__file__), '../Trajectories2/')
+
+    # directory = '/home/codmusic/Desktop/FOREST/Trajectories2/'
+    dances = []
+    trajectories = sorted(os.listdir(directory))
+    for filename in trajectories:
+        if filename.endswith(".csv"):
+            print(filename)
+            currentDance = (readFile(os.path.join(directory, filename)))
+            dances.append(currentDance)
+            continue
+
+    setup()
+    repeat = input("do we need to repeat? [y/n]")
+    if repeat == 'y':
+        setup()
+    for a in arms:
+        a.set_mode(1)
+        a.set_state(0)
+
+    try:
+        with conn:
+            s.settimeout(None)
+            print('Connected by', addr)
+            while True:
+                chord = conn.recv(1024).decode()
+                if chord:
+                    print(chord)
+                    if chord == "major":
+                        # playDance(dances[8])
+                        playDance(dances[22])
+                    if chord == "minor":
+                        # playDance(dances[13])
+                        playDance(dances[23])
+                    if chord == "augmented":
+                        playDance(dances[2])
+
+                    if chord == "diminished":
+                        playDance(dances[21])
+
+    except socket.error as socketerror:
+        s.close()  # Remember to close sockets after use!
+        print("Error: ", socketerror)
+
+    except KeyboardInterrupt:
+        s.close()  # Remember to close sockets after use!
+        print('Keyboard interrupt...')
+
+    finally:
+        s.close()  # Remember to close sockets after use!
+        print("Exiting...")
+
