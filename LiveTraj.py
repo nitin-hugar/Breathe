a953017f49c7fdf403a9d3d5e34d4de0859f9fb9
Hardik Goel <hardikkgoel@gmail.com>
2021-11-18 20:04:58 -0500

    Implemented access for all arm joints, need to fix efficiency

--- LiveTraj.py
+++ LiveTraj.py
@@ -7,12 +7,10 @@ from xarm.wrapper import XArmAPI
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-
-
 def setup():
     for a in arms:
         a.set_simulation_robot(on_off=False)
-        a.motion_enable(enable=True)
+        # a.motion_enable(enable=True)
         a.clean_warn()
         a.clean_error()
         a.set_mode(0)
@@ -24,7 +22,7 @@ def setup():
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    arm6 = XArmAPI('192.168.1.242')
+    arm6 = XArmAPI('192.168.1.211')
     arms = [arm6]
     setup()
     repeat = input("do we need to repeat? [y/n]")
@@ -35,15 +33,27 @@ if __name__ == '__main__':
     arm6.set_state(0)
     def changeDir():
         while True:
-            angle = int(input("desired angle"))
-            q.put(angle)
+            list = []
+            for i in range(0, 7):
+                angle = int(input(f'desired angle for joint {7 - i} : '))
+                list.append(angle)
+            que.put(list)
 
-    q = queue.Queue()
-    t2 = Thread(target=changeDir)
-    t2.start()
+    que = queue.Queue()
+    # t2 = Thread(target=changeDir)
+    # t2.start()
     IP = [0, 0, 0, 90, 0, 0, 0]
     tf = 3
 
+    pos1 = [0, 0, 0, 90, 0, 0, 0]
+    pos2 = [0, 30, 0, 90, 0, 45, 90]
+    pos3 = [0, 0, 0, 90, 0, 45, 0]
+
+    que.put(pos2)
+    que.put(pos3)
+
+    print("Queued position 2")
+    time.sleep(3)
 
     t0 = 0
     t = t0
@@ -56,53 +66,64 @@ if __name__ == '__main__':
     p = 0
     v = 0
     a = 0
+
+    j_angles=[0, 0, 0, 90, 0, 0, 0]
+
     while True:
-        goal = q.get()
-        q_i = p
-        q_dot_i = 0
-        q_dotdot_i = 0
-        q_f = goal
-        i = 0
+        q = que.get()
+        for joint in range(0, 7):
+            goal = q[6 - joint]
 
-        while i <= len(t_array):
-            start_time = time.time()
-            if q.empty() == False:
-                goal = q.get()
-                q_i = p
-                q_dot_i = v
-                q_dotdot_i = 0
-                q_f = goal
-                i = 0
-                # IF YOU WANT TO ADD SPEED CHANGES THEN SWAP THE ABOVE LINES WITH THE BELOW LINES
-                # # q should input an array of [*absolute* position of joint, time(in seconds) to reach there]
-                # q_f = goal[0]
-                # tf = goal[1]
-                # t_array = np.arange(0, tf, 0.006)
-                print("switch")
-            if i == len(t_array):
-                t = tf
-            else:
-                t = t_array[i]
-            a0 = q_i
-            a1 = q_dot_i
-            a2 = 0.5 * q_dotdot_i
-            a3 = 1.0 / (2.0 * tf**3.0) * (20.0 * (q_f - q_i) - (8.0 * q_dot_f + 12.0 * q_dot_i) * tf - (3.0 * q_dotdot_f - q_dotdot_i) * tf**2.0)
-            a4 = 1.0 / (2.0 * tf**4.0) * (30.0 * (q_i - q_f) + (14.0 * q_dot_f + 16.0 * q_dot_i) * tf + (3.0 * q_dotdot_f - 2.0 * q_dotdot_i) * tf**2.0)
-            a5 = 1.0 / (2.0 * tf**5.0) * (12.0 * (q_f - q_i) - (6.0 * q_dot_f + 6.0 * q_dot_i) * tf - (q_dotdot_f - q_dotdot_i) * tf**2.0)
+            # q_i = p
+            q_i = j_angles[6-joint]
+            q_dot_i = 0
+            q_dotdot_i = 0
+            q_f = goal
+            i = 0
+            print(joint)
+            while i <= len(t_array):
+                start_time = time.time()
+                if joint > 6:
+                    goal = q[6 - joint]
+                    q_i = p
+                    q_dot_i = v
+                    q_dotdot_i = 0
+                    q_f = goal
+                    i = 0
+                    # IF YOU WANT TO ADD SPEED CHANGES THEN SWAP THE ABOVE LINES WITH THE BELOW LINES
+                    # # q should input an array of [*absolute* position of joint, time(in seconds) to reach there]
+                    # q_f = goal[0]
+                    # tf = goal[1]
+                    # t_array = np.arange(0, tf, 0.006)
+                    print("switch")
+                if i == len(t_array):
+                    t = tf
+                else:
+                    t = t_array[i]
+                a0 = q_i
+                a1 = q_dot_i
+                a2 = 0.5 * q_dotdot_i
+                a3 = 1.0 / (2.0 * tf**3.0) * (20.0 * (q_f - q_i) - (8.0 * q_dot_f + 12.0 * q_dot_i) * tf - (3.0 * q_dotdot_f - q_dotdot_i) * tf**2.0)
+                a4 = 1.0 / (2.0 * tf**4.0) * (30.0 * (q_i - q_f) + (14.0 * q_dot_f + 16.0 * q_dot_i) * tf + (3.0 * q_dotdot_f - 2.0 * q_dotdot_i) * tf**2.0)
+                a5 = 1.0 / (2.0 * tf**5.0) * (12.0 * (q_f - q_i) - (6.0 * q_dot_f + 6.0 * q_dot_i) * tf - (q_dotdot_f - q_dotdot_i) * tf**2.0)
 
-            p = a0 + a1*t + a2*t**2 + a3*t**3 + a4*t**4 + a5*t**5
-            v = a1 + 2*a2*t + 3*a3*t**2 + 4*a4*t**3 + 5*a5*t**4
-            a = 2*a2 + 6*a3*t + 12*a4*t**2 + 20*a5*t**3
-            arm6.set_servo_angle_j(angles=[0, 0, 0, 90, 0, p, 0], is_radian=False)
-            tts = time.time() - start_time
-            sleep = 0.006 - tts
+                p = a0 + a1*t + a2*t**2 + a3*t**3 + a4*t**4 + a5*t**5
+                v = a1 + 2*a2*t + 3*a3*t**2 + 4*a4*t**3 + 5*a5*t**4
+                a = 2*a2 + 6*a3*t + 12*a4*t**2 + 20*a5*t**3
+                j_angles[6 - joint] = p
+                # print(j_angles)
+                # arm6.set_servo_angle_j(angles=[0, 0, 0, 90, 0, p, 0], is_radian=False)
+                arm6.set_servo_angle_j(angles=j_angles, is_radian=False)
+                tts = time.time() - start_time
+                sleep = 0.006 - tts
 
-            if tts > 0.006:
-                sleep = 0
+                if tts > 0.006:
+                    sleep = 0
 
-            #print(tts)
-            time.sleep(sleep)
-            i += 1
-            #if t == 1:
-                #print(t, p, v, a)
-        print("done")
+                #print(tts)
+                time.sleep(sleep)
+                i += 1
+                #if t == 1:
+                    #print(t, p, v, a)
+            print("done")
+            time.sleep(0.5)
