dd1cabbff3a1e908901fc3915dec720e20aba9eb
Hardik Goel <hgoel7@gatech.edu>
2021-12-03 11:59:59 -0500

    Implemented multiple arms, in progress

--- ChordLiveTraj.py
+++ ChordLiveTraj.py
@@ -23,122 +23,103 @@ def setup():
         a.set_state(0)
         a.set_servo_angle(angle=[0.0, 0.0, 0.0, 1.57, 0.0, 0, 0.0], wait=False, speed=0.4, acceleration=0.25, is_radian=True)
 
-def cvt(path):
-    return_vector = []
-    for i in range(7 * len(arms)):
-        return_vector.append(float(path[i]))
-    return return_vector
+ def playRobot(que, arm):
+        IP = [0, 0, 0, 90, 0, 0, 0]
+        tf = 3
 
-def readFile(csvFile):
-    flower = []
-    with open(csvFile, newline='') as csvfile:
-        paths_reader_j = csv.reader(csvfile, delimiter=',', quotechar='|')
-        for path in paths_reader_j:
-            flower.append(cvt(path))
-    return flower
+        # t0 = 0
+        t = [0 for i in range(0, 7)]
+        q_i = [0 for i in range(0, 7)]
+        q_dot_i = [0 for i in range(0, 7)]
+        q_dot_f = [0 for i in range(0, 7)]
+        q_dotdot_i = [0 for i in range(0, 7)]
+        q_dotdot_f = [0 for i in range(0, 7)]
+        t_array = np.arange(0, tf, 0.006)
+        p = [0, 0, 0, 90, 0, 0, 0]
+        v = [0 for i in range(0, 7)]
+        a = [0 for i in range(0, 7)]
 
-def playDance(step, que):
-    length = len(step)
+        # j_angles = [0, 0, 0, 90, 0, 0, 0]
 
-    for i in range(length):
-        start_time = time.time()
-        j_angles = (step[i])
+        while True:
+            q = que.get()
 
-        que.put(j_angles)
+            # print("\nDequeued")
+            goal = q
+            q_i = p
+            # q_i = j_angles
+            q_dot_i = [0, 0, 0, 0, 0, 0, 0]
+            q_dotdot_i = [0, 0, 0, 0, 0, 0, 0]
+            q_f = goal
+            j = 0
 
-        tts = time.time() - start_time
-        sleep = 0.006 - tts
+            while j <= len(t_array):
+                start_time = time.time()
 
-        if tts > 0.006:
-            sleep = 0
+                if que.empty() == False:
+                    goal = q.get()
+                    q_i = p
+                    q_dot_i = v
+                    q_dotdot_i = [0 for i in range(0, 7)]
+                    q_f = goal
+                    j = 0
+                    # IF YOU WANT TO ADD SPEED CHANGES THEN SWAP THE ABOVE LINES WITH THE BELOW LINES
+                    # # q should input an array of [*absolute* position of joint, time(in seconds) to reach there]
+                    # q_f = goal[0]
+                    # tf = goal[1]
+                    # t_array = np.arange(0, tf, 0.006)
+                    print("switch")
+                if j == len(t_array):
+                    t = tf
+                else:
+                    t = t_array[j]
 
-        print(tts)
-        time.sleep(sleep)
+                a0 = q_i
+                a1 = q_dot_i
+                a2 = []
+                a3 = []
+                a4 = []
+                a5 = []
 
-def playRobot(que, arm):
-    IP = [0, 0, 0, 90, 0, 0, 0]
-    tf = 3
+                for i in range(0, 7):
+                    a2.append(0.5 * q_dotdot_i[i])
+                    a3.append(1.0 / (2.0 * tf ** 3.0) * (
+                                20.0 * (q_f[i] - q_i[i]) - (8.0 * q_dot_f[i] + 12.0 * q_dot_i[i]) * tf - (
+                                    3.0 * q_dotdot_f[i] - q_dotdot_i[i]) * tf ** 2.0))
+                    a4.append(1.0 / (2.0 * tf ** 4.0) * (
+                                30.0 * (q_i[i] - q_f[i]) + (14.0 * q_dot_f[i] + 16.0 * q_dot_i[i]) * tf + (
+                                    3.0 * q_dotdot_f[i] - 2.0 * q_dotdot_i[i]) * tf ** 2.0))
+                    a5.append(1.0 / (2.0 * tf ** 5.0) * (
+                                12.0 * (q_f[i] - q_i[i]) - (6.0 * q_dot_f[i] + 6.0 * q_dot_i[i]) * tf - (
+                                    q_dotdot_f[i] - q_dotdot_i[i]) * tf ** 2.0))
 
-    # t0 = 0
-    t = [0 for i in range(0, 7)]
-    q_i = [0 for i in range(0, 7)]
-    q_dot_i = [0 for i in range(0, 7)]
-    q_dot_f = [0 for i in range(0, 7)]
-    q_dotdot_i = [0 for i in range(0, 7)]
-    q_dotdot_f = [0 for i in range(0, 7)]
-    t_array = np.arange(0, tf, 0.006)
-    p = [0, 0, 0, 90, 0, 0, 0]
-    v = [0 for i in range(0, 7)]
-    a = [0 for i in range(0, 7)]
+                    p[i] = (a0[i] + a1[i] * t + a2[i] * t ** 2 + a3[i] * t ** 3 + a4[i] * t ** 4 + a5[i] * t ** 5)
+                    v[i] = (a1[i] + 2 * a2[i] * t + 3 * a3[i] * t ** 2 + 4 * a4[i] * t ** 3 + 5 * a5[i] * t ** 4)
+                    a[i] = (2 * a2[i] + 6 * a3[i] * t + 12 * a4[i] * t ** 2 + 20 * a5[i] * t ** 3)
 
-    # j_angles = [0, 0, 0, 90, 0, 0, 0]
+                j_angles = p
+                arm.set_servo_angle_j(angles=p, is_radian=False)
+                # print(f"{p} {arm}")
 
+                tts = time.time() - start_time
+                sleep = 0.006 - tts
+
+                if tts > 0.006:
+                    sleep = 0
+
+                time.sleep(sleep)
+                j += 1
+                # if t == 1:
+                # print(t, p, v, a)
+
+            print(f"\nFinished {arm}")
+
+def changeDir(lst):
     while True:
-        q = que.get()
+        pos = list(map(int, input("\nEnter the positions for joints 1-7 : ").strip().split()))[:7]
+        for que in lst:
+            que.put(pos)
 
-        # print("\nDequeued")
-        goal = q
-        q_i = p
-        # q_i = j_angles
-        q_dot_i = [0, 0, 0, 0, 0, 0, 0]
-        q_dotdot_i = [0, 0, 0, 0, 0, 0, 0]
-        q_f = goal
-        j = 0
-
-        while j <= len(t_array):
-            start_time = time.time()
-
-            if que.empty() == False:
-                goal = q.get()
-                q_i = p
-                q_dot_i = v
-                q_dotdot_i = [0 for i in range(0, 7)]
-                q_f = goal
-                j = 0
-                print("switch")
-            if j == len(t_array):
-                t = tf
-            else:
-                t = t_array[j]
-
-            a0 = q_i
-            a1 = q_dot_i
-            a2 = []
-            a3 = []
-            a4 = []
-            a5 = []
-
-            for i in range(0, 7):
-                a2.append(0.5 * q_dotdot_i[i])
-                a3.append(1.0 / (2.0 * tf ** 3.0) * (
-                        20.0 * (q_f[i] - q_i[i]) - (8.0 * q_dot_f[i] + 12.0 * q_dot_i[i]) * tf - (
-                        3.0 * q_dotdot_f[i] - q_dotdot_i[i]) * tf ** 2.0))
-                a4.append(1.0 / (2.0 * tf ** 4.0) * (
-                        30.0 * (q_i[i] - q_f[i]) + (14.0 * q_dot_f[i] + 16.0 * q_dot_i[i]) * tf + (
-                        3.0 * q_dotdot_f[i] - 2.0 * q_dotdot_i[i]) * tf ** 2.0))
-                a5.append(1.0 / (2.0 * tf ** 5.0) * (
-                        12.0 * (q_f[i] - q_i[i]) - (6.0 * q_dot_f[i] + 6.0 * q_dot_i[i]) * tf - (
-                        q_dotdot_f[i] - q_dotdot_i[i]) * tf ** 2.0))
-
-                p[i] = (a0[i] + a1[i] * t + a2[i] * t ** 2 + a3[i] * t ** 3 + a4[i] * t ** 4 + a5[i] * t ** 5)
-                v[i] = (a1[i] + 2 * a2[i] * t + 3 * a3[i] * t ** 2 + 4 * a4[i] * t ** 3 + 5 * a5[i] * t ** 4)
-                a[i] = (2 * a2[i] + 6 * a3[i] * t + 12 * a4[i] * t ** 2 + 20 * a5[i] * t ** 3)
-
-            # j_angles = p
-            arm.set_servo_angle_j(angles=p, is_radian=False)
-
-            tts = time.time() - start_time
-            sleep = 0.006 - tts
-
-            if tts > 0.006:
-                sleep = 0
-
-            time.sleep(sleep)
-            j += 1
-            # if t == 1:
-            # print(t, p, v, a)
-
-        # print("\nFinished")
 
 if __name__ == "__main__":
     # INITIALIZE Socket
@@ -171,23 +152,6 @@ if __name__ == "__main__":
     arms = [arm1, arm2, arm3, arm4]
     totalArms = len(arms)
 
-    # Load Dances
-    directory = os.path.join(os.path.dirname(__file__), '../Trajectories2/')
-    dances = []
-    trajectories = sorted(os.listdir(directory))
-    for filename in trajectories:
-        if filename.endswith(".csv"):
-            print(filename)
-            currentDance = (readFile(os.path.join(directory, filename)))
-            dances.append(currentDance)
-            continue
-
-    # Start Arms
-    t1 = Thread(target=playRobot, args=(a1, arm1,))
-    t2 = Thread(target=playRobot, args=(a2, arm2,))
-    t3 = Thread(target=playRobot, args=(a3, arm3,))
-    t4 = Thread(target=playRobot, args=(a4, arm4,))
-
     setup()
     repeat = input("do we need to repeat? [y/n]")
     if repeat == 'y':
@@ -195,6 +159,12 @@ if __name__ == "__main__":
     for a in arms:
         a.set_mode(1)
         a.set_state(0)
+
+    # Start Arms
+    t1 = Thread(target=playRobot, args=(a1, arm1,))
+    t2 = Thread(target=playRobot, args=(a2, arm2,))
+    t3 = Thread(target=playRobot, args=(a3, arm3,))
+    t4 = Thread(target=playRobot, args=(a4, arm4,))
 
     t1.start()
     t2.start()
@@ -209,25 +179,6 @@ if __name__ == "__main__":
                 chord = conn.recv(1024).decode()
                 if chord:
                     print(chord)
-                    if chord == "major":
-                        playDance(dances[22], a1)
-                        playDance(dances[22], a2)
-
-                    if chord == "minor":
-                        playDance(dances[23], a3)
-                        playDance(dances[23], a4)
-
-                    if chord == "augmented":
-                        playDance(dances[2], a1)
-                        playDance(dances[2], a2)
-                        playDance(dances[2], a3)
-                        playDance(dances[2], a4)
-
-                    if chord == "diminished":
-                        playDance(dances[21], a1)
-                        playDance(dances[21], a2)
-                        playDance(dances[21], a3)
-                        playDance(dances[21], a4)
 
     except socket.error as socketerror:
             s.close()  # Remember to close sockets after use!
